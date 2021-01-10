cdb4cb27480e140410ec66e0a58ddf19582357eb
Flo <37000563+flok@users.noreply.github.com>
2021-01-10 14:50:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='hidapi-usb',
-    version="0.2.4",
+    version="0.2.5",
     description=("CFFI wrapper for hidapi with changes by flok"),
     author="Florian K",
     url="https://github.com/flok/hidapi-cffi.git",
     author_email="37000563+flok@users.noreply.github.com",
     license='BSD',
     py_modules=['hidapi'],
-    setup_requires=['cffi >= 1.14.4']
+    install_requires=['cffi']
 )
